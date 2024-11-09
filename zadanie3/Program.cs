c07e4c362700e814714653cedeb3da8df97f2604
mardofff <88830456+mardofff@users.noreply.github.com>
2024-11-09 18:46:16 +0100

    Add files via upload

---
+++ zadanie3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace SystemBankowy
+{
+    public class KontoBankowe
+    {
+        String Wlasciciel { get; set; }
+        decimal rn { get; set; }
+
+        public KontoBankowe(string wlasciciel)
+        {
+            Wlasciciel = wlasciciel;
+            rn = 0;
+        }
+
+        public void Wplacanie(decimal kwota)
+        {
+            if (kwota < 0)
+            {
+                Console.WriteLine("Kwota musi byc wieksza od zera");
+                return;
+            }
+
+            rn += kwota;
+            Console.WriteLine("Na konto wplacono" + kwota + " Nowy stan konta: " + rn);
+        }
+
+        public void Wyplacanie(decimal kwota)
+        {
+            if (kwota <= 0)
+            {
+                Console.WriteLine("Kwota musi byc wieksza od zera");
+                return;
+            }
+            {
+                Console.WriteLine("Niemoza wyplacic, jest za malo pieniedzy na koncie");
+                return;
+            }
+
+            rn -= kwota;
+            Console.WriteLine("Z konta wyplacono: " + kwota + " Nowy stan konta: " + rn);
+
+        }
+        public void Lokata()
+        {
+            decimal odsetki = rn * 0.05m;
+            rn += odsetki;
+            Console.WriteLine("Do konta z odsetek dodano: " + odsetki + " Stan konta po dodaniu: " + rn);
+        }
+
+        class Bank
+        {
+            static void Main(string[] args)
+            {
+                KontoBankowe konto = new KontoBankowe("Maria Osypenko");
+
+                konto.Wyplacanie(5);
+                konto.Wplacanie(10);
+                konto.Wyplacanie(50);
+
+                konto.Lokata();
+                Console.WriteLine("Koncowy stan konta " + konto.Wlasciciel + " wynosi: " + konto.rn);
+
+            }
+        }
+    }
+}
---
+++ zadanie4/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+public class KontoBankowe
+{
+    public string Wlasciciel { get; private set; }
+    public decimal Stan { get; private set; }
+
+    public KontoBankowe(string wlasciciel)
+    {
+        Wlasciciel = wlasciciel;
+        Stan = 0; 
+    }
+
+    public void Wplac(decimal kwota)
+    {
+        if (kwota <= 0)
+        {
+            Console.WriteLine("Kwota musi byc wieksza od zera");
+            return;
+        }
+        Stan += kwota;
+        Console.WriteLine($"Wplacono: {kwota:C}. Nowy stan: {Stan:C}.");
+    }
+
+    public void Wyplac(decimal kwota)
+    {
+        if (kwota <= 0)
+        {
+            Console.WriteLine("Kwota musi byc wieksza od zera");
+            return;
+        }
+        if (kwota > Stan)
+        {
+            Console.WriteLine("Niewystarczajace srodki na koncie");
+            return;
+        }
+        Stan -= kwota;
+        Console.WriteLine($"Wyplacono: {kwota:C}. Nowy stan: {Stan:C}.");
+    }
+
+    public void SprawdzStan()
+    {
+        Console.WriteLine($"Aktualny stan konta {Wlasciciel}: {Stan:C}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KontoBankowe konto = new KontoBankowe("Maria Osypenko");
+        bool dalej = true;
+
+        while (dalej)
+        {
+            Console.WriteLine("\nWybierz operację:");
+            Console.WriteLine("1. Wplata");
+            Console.WriteLine("2. Wyplata");
+            Console.WriteLine("3. Sprawdz stan konta");
+            Console.WriteLine("4. Zakoncz");
+
+            string wybor = Console.ReadLine(); //wczytywanie danych
+
+            switch (wybor)
+            {
+                case "1":
+                    Console.Write("Podaj kwote do wplaty: ");
+                    if (decimal.TryParse(Console.ReadLine(), out decimal kwotaWplaty))
+                    {
+                        konto.Wplac(kwotaWplaty);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nieprawidlowa kwota");
+                    }
+                    break;
+
+                case "2":
+                    Console.Write("Podaj kwote do wyplaty: ");
+                    if (decimal.TryParse(Console.ReadLine(), out decimal kwotaWyplaty))
+                    {
+                        konto.Wyplac(kwotaWyplaty);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nieprawidlowa kwota");
+                    }
+                    break;
+
+                case "3":
+                    konto.SprawdzStan();
+                    break;
+
+                case "4":
+                    dalej = false;
+                    Console.WriteLine("no to koniec ");
+                    break;
+
+                default:
+                    Console.WriteLine("Sprobuj ponownie!!!!!");
+                    break;
+            }
+        }
+    }
+}
